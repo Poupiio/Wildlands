8a83ff012aa3396795e5124c7b983a06a7f69032
Elise <stagiaireinfo2@avista.fr>
2023-06-16 22:05:55 +0200

    Ajout Bootstrap + test immonde

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import style from '../modules/Header.module.css';
+
 const Header = () => {
     return (
         <header>
-            <nav>
+            <Nav className={style.navBar}>
+                <Container>
+                    <Nav className="me-auto justify-content-around">
+                        <Nav.Link className={style.navLink} href="#home">Accueil</Nav.Link>
+                        <Nav.Link className={style.navLink} href="#home">Safaris</Nav.Link>
+                        <Nav.Link className={style.navLink} href="#features">Faune et Flore</Nav.Link>
+                        <Nav.Link className={style.navLink} href="#pricing">Galerie</Nav.Link>
+                        <Nav.Link className={style.navLink} href="#pricing">Connexion</Nav.Link>
+                    </Nav>
+                </Container>
+            </Nav>
+            {/* <nav>
                 <ul>
                     <li>Accueil</li>
                     <li>Safaris</li>
@@ -9,7 +25,7 @@ const Header = () => {
                     <li>Galerie</li>
                     <li>Connexion</li>
                 </ul>
-            </nav>
+            </nav> */}
         </header>
     )
 }
